a4b2b347d5135b5c7ce9754577203c1813cbddf8
suresh bethanasamy <suresh.b@WSEINML-GSRZFVH.local>
2020-11-20 15:03:18 +0530

    Loaders added for assets load and SASS integrated

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,37 @@ module.exports = {
                 }
             },
             {
-                "test": /\.css$/,
-                "use": [
-                    "style-loader",
-                    "css-loader"
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
                 ]
+            },
+            {
+                "test": /\.s[ac]ss$/i,
+                "use": [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192 // in bytes
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                  loader: 'url-loader',
+                },
             },
             {
                 "test": /\.html$/,
@@ -50,5 +76,9 @@ module.exports = {
             template: "./src/index.html",
             filename: "./index.html"
         })
-    ]
+    ],
+    devServer: {
+        inline:true,
+        port: 4949
+    },
 }
